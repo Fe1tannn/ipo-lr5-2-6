1c257aa19e069155b106bebbd76627e585fc00b7
Yan <tal531764@gmail.com>
2024-11-19 16:21:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ with open ("text.txt", "r", encoding="utf-8") as file:
     b = a.split()
     with open ("output.txt", "w", encoding="utf-8") as file1:
         for i in b:
-            if len(i) == 5:
+            if len(i) > 5:
                 file1.write(str(i))
