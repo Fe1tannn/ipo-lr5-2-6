73663d0d591d04ca9a3f57c2996555908e59c8f2
Yan <tal531764@gmail.com>
2024-11-19 16:21:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 with open ("text.txt", "r", encoding="utf-8") as file:
     a = file.read()
     b = a.split()
-    c = []
-    for i in b:
-        if len(i) == 5:
-            c.append()
     with open ("output.txt", "w", encoding="utf-8") as file1:
-        file1.write(c)
+        for i in b:
+            if len(i) == 5:
+                file1.write(str(i))
