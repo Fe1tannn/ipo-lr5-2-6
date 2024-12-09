836a47237c57f3871367285af641dacf06439d09
Yan <tal531764@gmail.com>
2024-12-10 01:21:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-with open ("text.txt", "r", encoding="utf-8") as file:
-    a = file.read()
-    b = a.split()
-    with open ("output.txt", "w", encoding="utf-8") as file1:
-        for i in b:
-            if len(i) > 5:
-                file1.write(str(i))
+with open ("text.txt", "r", encoding="utf-8") as file: #Открываем файл
+    a = file.read() #Сохраняем данные файла в переменной a
+    b = a.split() #Сохраняем слова в список b
+    with open ("output.txt", "w", encoding="utf-8") as file1: #Открываем файл
+        for i in b: #Перебор
+            if len(i) > 5: #Условие
+                file1.write(str(i)) #Записываем элемент i в файл
